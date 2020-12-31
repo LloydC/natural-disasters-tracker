47cef321f9e3fe1064bdcb11ca7a0c13679ab5d4
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-12-31 16:20:57 +0100

    Changed some content

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <header className="header">
-            <h1>Natural Disasters Tracker (Powered By NASA)</h1>
+            <h1>Natural Disasters Tracker (Powered By NASA API)</h1>
         </header>
     )
 }
--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -3,10 +3,9 @@ import wildfireIcon from '@iconify/icons-mdi/fire-alert'
 import volcanoIcon from '@iconify-icons/noto/volcano';
 import cycloneIcon from '@iconify-icons/noto/cyclone';
 
-const LocationMarker = ({ lat, long, type, onClick}) => {
+const LocationMarker = ({ type, onClick}) => {
     return (
         <div className="location-marker" onClick={onClick}>
-            <Icon icon={type === 'eruption'? volcanoIcon : wildfireIcon} className="location-icon"/>
             {type === 'wildfire' && <Icon icon={wildfireIcon} className="location-icon"/>}
             {type === 'eruption' && <Icon icon={volcanoIcon} className="location-icon"/>}
             {type === 'cyclone' && <Icon icon={cycloneIcon} className="location-icon"/>}
