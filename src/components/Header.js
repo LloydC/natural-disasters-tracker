bcf78e2463213e7e291f62bbaa412260f336979d
Lloyd Chambrier <lloydchambrier@gmail.com>
2021-01-05 21:09:14 +0100

    Changed Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Icon } from '@iconify/react';
+import wildfireIcon from '@iconify/icons-mdi/fire-alert'
+import volcanoIcon from '@iconify-icons/noto/volcano';
+import cycloneIcon from '@iconify-icons/noto/cyclone';
 
 const Header = () => {
     return (
         <header className="header">
             <h1>Natural Disasters Tracker (Powered By NASA API)</h1>
+            <div>
+                <span><Icon icon={volcanoIcon} className="location-icon"/> Volcanoes</span>
+                <span><Icon icon={wildfireIcon} className="location-icon"/> Wildfire</span>
+                <span><Icon icon={cycloneIcon} className="location-icon"/> Cyclone</span>
+            </div>
         </header>
     )
 }
