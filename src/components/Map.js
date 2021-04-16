41d209898d64d72c061e98876758345eb9aba16e
Lloyd Chambrier <lloydchambrier@gmail.com>
2021-04-16 19:09:52 +0000

    More styling changes

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,10 +4,10 @@ import LocationMarker from './LocationMarker';
 import LocationInfoBox from './LocationInfoBox';
 import Header from './Header';
 import Footer from './Footer';
-// import { Icon } from '@iconify/react';
-// import wildfireIcon from '@iconify/icons-mdi/fire-alert'
-// import volcanoIcon from '@iconify-icons/noto/volcano';
-// import cycloneIcon from '@iconify-icons/noto/cyclone';
+import { Icon } from '@iconify/react';
+import wildfireIcon from '@iconify/icons-mdi/fire-alert'
+import volcanoIcon from '@iconify-icons/noto/volcano';
+import cycloneIcon from '@iconify-icons/noto/cyclone';
 
  const Map = ({ eventData, center, zoom }) => {
      const [locationInfo, setLocationInfo] = useState(null)
@@ -52,13 +52,23 @@ import Footer from './Footer';
                 >
                     {markers}
                 </GoogleMapReact>
-                {/* <div>
-                    <span style={{border: '1px solid black', borderRadius: '15px', marginLeft: '10px'}}><Icon icon={volcanoIcon} className="location-icon"/> Volcanoes</span>
-                    <span style={{border: '1px solid black', borderRadius: '15px', marginLeft: '10px'}}><Icon icon={wildfireIcon} className="location-icon"/> Wildfire</span>
-                    <span style={{border: '1px solid black', borderRadius: '15px', marginLeft: '10px'}}><Icon icon={cycloneIcon} className="location-icon"/> Cyclone</span>
-                </div> */}
+                <div className="info-container">
+                    <div>
+                        <div style={{textAlign: "center"}}>  
+                            <h2>Welcome!</h2>
+                            <p>You can click on any of the disasters icons that appears on the map to get more information about it ;)</p>
+                        </div>
+                        <br/>
+                        <div style={{ display: "flex", flexDirection: "column"}}>
+                            <span><Icon icon={volcanoIcon} className="location-icon"/> Volcanoes</span>
+                            <span><Icon icon={wildfireIcon} className="location-icon"/> Wildfire</span>
+                            <span><Icon icon={cycloneIcon} className="location-icon"/> Cyclone</span>
+                        </div> 
+                    </div>
+                    
+                    {locationInfo && <LocationInfoBox info={locationInfo}/>}
+                </div>
             </div>
-            {locationInfo && <LocationInfoBox info={locationInfo}/>}
             <Footer/>
         </div>
     )
